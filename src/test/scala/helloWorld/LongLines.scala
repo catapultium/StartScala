d6567c4507502ec70449be67b19018a371e836d3
whale-so <whale.so@kakaocorp.com>
2018-02-07 19:02:26 +0900

    [feat]함수 내부에 있으므로 파라미터를 모두 전달할 필요없다.

--- src/test/scala/helloWorld/LongLines.scala
+++ src/test/scala/helloWorld/LongLines.scala
@@ -19,13 +19,13 @@ object LongLines {
   /* 함수 안에 함수를 쓸 수 있다. private 역할을 대신할 수 도 있으며 블록 내에서만 사용하므로 깔끔하다 */
   def processFile2(filename: String, width: Int) {
 
-    def processLine(filename: String, width: Int, line: String): Unit = {
+    def processLine(line: String): Unit = {
       if (line.length > width)
         println(filename + ": " + line.trim)
     }
 
     val source = Source.fromFile(filename)
     for (line <- source.getLines())
-      processLine(filename, width, line)
+      processLine(line)
   }
 }
