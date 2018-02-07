4847c90509be5006f8401b42701e80c146cdba60
whale-so <whale.so@kakaocorp.com>
2018-02-07 11:03:15 +0900

    [feat]add method 추가.

--- src/test/scala/rational/Main.scala
+++ src/test/scala/rational/Main.scala
@@ -2,7 +2,12 @@ package rational
 
 object Main {
   def main(args: Array[String]): Unit = {
-    println(new Rational(1, 2).toString)
-    println(new Rational(1, 0).toString)
+    val oneHalf = new Rational(1, 2)
+    val twoThirds = new Rational(2, 3)
+    println(oneHalf add twoThirds).toString
+
+    /* 공개 필드이므로 접근 가능 */
+    println(oneHalf.numer)
+    println(oneHalf.denom)
   }
 }
--- src/test/scala/rational/Rational.scala
+++ src/test/scala/rational/Rational.scala
@@ -8,6 +8,14 @@ class Rational(n: Int, d: Int) {
   * Predef 독립 객체에 들어있음 */
   require(d != 0)
 
+  /* 메소드 스코프에 바로 접근할 수 없기 떄문에 필드를 추가한다. */
+  val numer: Int = n
+  val denom: Int = d
+
   /* 지정하지 않으면 클래스이름@16진수숫자 */
   override def toString: String = n + "/" + d
+
+  def add(that: Rational): Rational = new Rational(
+    numer * that.denom + that.numer * denom,
+    denom * that.denom)
 }
