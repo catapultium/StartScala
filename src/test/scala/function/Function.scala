ac8f981bf222ea6502a5a4d0d79c1feaa6338ea3
whale-so <whale.so@kakaocorp.com>
2018-02-07 19:32:52 +0900

    [feat]함수 리터럴의 사용

--- src/test/scala/function/Function.scala
+++ src/test/scala/function/Function.scala
@@ -13,5 +13,14 @@ object Function {
       println()
       LongLines.processFile2(arg, width)
     }
+
+    /* 함수 리터럴 */
+    val increase = (x: Int) => x + 1
+    println(increase(10))
+
+    val someNumbers = List(-11, -10, -5, 0, 5, 10)
+    someNumbers.foreach((x: Int) => println(x))
+    someNumbers.filter((x: Int) => x > 0).foreach((x: Int) => println(x))
+
   }
 }
