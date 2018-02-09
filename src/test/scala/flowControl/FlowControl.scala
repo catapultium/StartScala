ede527c50b69c595a9b803c1f6ec9883cb2a3489
whale-so <whale.so@kakaocorp.com>
2018-02-09 11:16:23 +0900

    [feat]loan pattern

--- src/test/scala/flowControl/FlowControl.scala
+++ src/test/scala/flowControl/FlowControl.scala
@@ -1,5 +1,7 @@
 package flowControl
 
+import java.io.{File, PrintWriter}
+
 object FlowControl {
   def main(args: Array[String]): Unit = {
 
@@ -38,6 +40,45 @@ object FlowControl {
 
     /* 함수 자리라는 것을 알기 때문에 위치표시자를 사용하여 바로 파라미터 사용이 가능하다. */
     println(twice(Math.PI * _, 1))
+
+
+    /* 이렇게 하면 해당 메소드가 파일 닫기를 보장한다. loan pattern 이라 부른다. */
+    def withPrintWriter(file: File, op: PrintWriter => Unit): Unit = {
+      val writer = new PrintWriter(file)
+      try {
+        op(writer)
+      } finally {
+        writer.close()
+      }
+    }
+
+    withPrintWriter(
+      new File("date.txt"),
+      writer => writer.println(new java.util.Date)
+    )
+
+
+    /* 스칼라에서는 제어 구조처럼 보이도록 하기 위해 인자가 하나인 경우 중괄호 사용이 가능하다 */
+    println("Hello world!")
+    println {
+      "Hello world!"
+    }
+
+    /* 커링을 이용 */
+    def withPrintWriter2(file: File)(op: PrintWriter => Unit): Unit = {
+      val writer = new PrintWriter(file)
+      try {
+        op(writer)
+      } finally {
+        writer.close()
+      }
+    }
+
+    /* 커링을 이용해 인자를 하나로 나눴기 때문에 중괄호 사용 가능 */
+    val file = new File("date.txt")
+    withPrintWriter2(file) {
+      writer => writer.println(new java.util.Date)
+    }
 
   }
 }
