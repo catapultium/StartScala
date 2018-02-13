189439be60b591eb5076ebf608e2a2f21de78791
whale-so <whale.so@kakaocorp.com>
2018-02-13 16:41:59 +0900

    [feat]trait override

--- src/test/scala/traitTest/Frog.scala
+++ src/test/scala/traitTest/Frog.scala
@@ -1,5 +1,11 @@
 package traitTest
 
-class Frog extends Philosophical {
+class Animal
+
+class Frog extends Animal with Philosophical {
   override def toString = "green"
+
+  override def philosophize(): Unit = {
+    println("It ain't easy being " + toString + "!")
+  }
 }
